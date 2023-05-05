7494426679188a4578dc4ec091e6315cfb6f1473
Shruthi.A <99746850+shruthibalu@users.noreply.github.com>
2023-05-05 13:02:28 +0530

    it is an updated version of calendar(GUI include)

--- calender.py
+++ calender.py
@@ -1,9 +1,18 @@
-import calendar
+from tkinter import *
+from tkcalendar import Calendar
+app=Tk()
+app.geometry("420x420")
+app.title("calender Date Picker App")
+app.config(bg="darkorange")
+calenderObj=Calendar(app,selectmode="day",year=2022,month=1,date=1)
+calenderObj.pack(pady=45)
+def fetch_date():
+    date.config(text="selected date is : "+calenderObj.get_date())
 
-# input year and month
-year = int(input("Enter the year: "))
-month = int(input("Enter the month (1-12): "))
 
-# print calendar
-print(calendar.month(year, month))
+button=Button(app,text="select Date",bg="black",fg="white",command=fetch_date)
+button.pack()
+date=Label(app,text="",bg="black",fg="white")
+date.pack(pady=20)
 
+app.mainloop()
